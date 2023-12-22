881b29dbef60761e0def6e91b935673ec0a5eebf
Julian So <ch.julian.so@gmail.com>
2023-12-22 08:15:15 -0500

    align scoreboard to right of screen

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Scoreboard from './Scoreboard';
+import '../../styles/gameboard.css';
 
 export default function Gameboard() {
   return (
     <>
       <div className='gameboard'>
-        <Scoreboard />
+        <div className='header'>
+          <Scoreboard />
+        </div>
       </div>
       ;
     </>
